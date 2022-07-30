a2a7169f6a7f03e623424b406467b911f9c01d5a
istareatscreens <schar138@uottawa.ca>
2022-07-30 01:22:33 -0400

    set no implicit any to false in tsconfig

---
+++ extension/src/types.d.ts
@@ -0,0 +1,27 @@
+import vscode = require("vscode");
+import path = require("path");
+import fs = require("fs");
+
+type FileData = {
+  scheme: string;
+  $mid: number;
+  external: vscode.Uri;
+  fsPath: string;
+  path: string;
+  schema: string;
+  _sep: number;
+};
+
+type WorkspaceFileList = [FileData, FileData[]];
+
+type Merge<A, B> = { [K in keyof (A | B)]: K extends keyof B ? B[K] : A[K] };
+
+type FileProfile = {
+  filename: string;
+  fileExtension: string;
+  fileBaseName: string;
+  filePath: string;
+  fileData: Buffer;
+  fileMetaData: fs.Stats;
+  isBinary: boolean;
+};
